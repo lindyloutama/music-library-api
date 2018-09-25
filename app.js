504e63737bc62e1376df9cb63be2dc12bb20ea4b
Linda <linda.tamakaha@gmail.com>
2018-09-25 23:20:32 +0100

    committing step 2 & 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Artist = require('./controllers/Artist');
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
@@ -11,5 +12,8 @@ const app = express();
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true });
 app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
+app.post('/Artist', Artist.post);
+app.get('/Artist', Artist.list);
+app.get('/Artist/:artistId', Artist.get);
 
 app.listen(3000, () => console.log('I am amazing!'));
